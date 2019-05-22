260289e412703315d69696fcbdf45297a0ba18d6
KoushiKagawa <koushi.kagawa@resinc.co.jp>
2019-05-22 12:01:30 +0900

    add GA tag

--- src/main.js
+++ src/main.js
@@ -2,27 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 
 /* font awesome */
-/*
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(faCoffee)
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-*/
-
-// Font Awesome のメインファイルを import
 
 import '@fortawesome/fontawesome-free-webfonts/css/fontawesome.css'
 
 // 使用するカテゴリーのファイルを import
-
 import '@fortawesome/fontawesome-free-webfonts/css/fa-brands.css'
 import '@fortawesome/fontawesome-free-webfonts/css/fa-regular.css'
 import '@fortawesome/fontawesome-free-webfonts/css/fa-solid.css'
 
-/* //font awesome */
+
+/* Google Analytics */
+import VueAnalytics from 'vue-analytics'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-140693200-1'
+})
+
 
 Vue.config.productionTip = false
 
